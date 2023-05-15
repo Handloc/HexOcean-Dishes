4ee4352ffb888fc6bf7fbb00d6658a6395043354
Maciej Handke <maciejhandke11@gmail.com>
2023-05-15 16:54:40 +0200

    Destructure register function from React Hook Form for all input fields

--- src/components/DishesForm.js
+++ src/components/DishesForm.js
@@ -1,18 +1,26 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 function DishesForm() {
+  const form = useForm();
+  const { register } = form;
   return (
-    <form>
-      <label HtmlFor="name">Name</label>
-      <input type="text" name="name" />
-      <label HtmlFor="preparation_time">Preparation time</label>
-      <input type="time" name="preparation_time" />
-      <label HtmlFor="type">Type</label>
-      <select name="option">
+    <form className="w-4/5 m-auto mt-20 bg-gray-500 flex flex-col border-2 border-black p-5 ">
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" {...register("name")} />
+      <label htmlFor="preparation_time">Preparation time</label>
+      <input
+        type="time"
+        id="preparation_time"
+        {...register("preparation_time")}
+      />
+      <label htmlFor="type">Type</label>
+      <select id="option" {...register("option")}>
         <option value="pizza">Pizza</option>
         <option value="soup">Soup</option>
         <option value="sandwich">Sandwich</option>
       </select>
+
       <button>Submit</button>
     </form>
   );
