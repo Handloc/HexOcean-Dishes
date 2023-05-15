9763181a9a701fdb58bf66928a050d4bece632c7
Maciej Handke <maciejhandke11@gmail.com>
2023-05-15 20:13:41 +0200

    Conditionally render details fields

--- src/components/DishesForm.js
+++ src/components/DishesForm.js
@@ -7,10 +7,15 @@ function DishesForm() {
       name: "",
       preparation_time: "00:00:00",
       type: "pizza",
+      no_of_slices: "",
+      diameter: "",
+      spiciness_scale: 5,
+      slices_of_bread: "",
     },
   });
-  const { register, handleSubmit, formState } = form;
+  const { register, handleSubmit, formState, watch } = form;
   const { errors } = formState;
+  const type = watch("type");
 
   function submitHandler(data) {
     console.log(data);
@@ -45,8 +50,7 @@ function DishesForm() {
           required: { value: true, message: "Preparation time is required" },
           pattern: {
             value: /^(?:[0-9][0-9]):(?:[0-9][0-9]):(?:[0-9][0-9])$/,
-            message:
-              "Preparation time format is invalid. It should be HH:MM:SS",
+            message: "Proper format is HH:MM:SS",
           },
           validate: (fieldValue) => {
             return (
@@ -66,6 +70,70 @@ function DishesForm() {
         <option value="sandwich">Sandwich</option>
       </select>
 
+      {type === "pizza" && (
+        <>
+          <label htmlFor="no_of_slices">Number of slices</label>
+          <input
+            type="number"
+            id="no_of_slices"
+            {...register("no_of_slices", {
+              required: {
+                value: true,
+                message: "Number of slices is required",
+              },
+            })}
+          />
+          <p className="text-red-300 font-bold">
+            {errors.no_of_slices?.message}
+          </p>
+          <label htmlFor="diameter">Diameter</label>
+          <input
+            type="number"
+            step="0.01"
+            id="diameter"
+            {...register("diameter", {
+              required: { value: true, message: "Diameter is required" },
+            })}
+          />
+          <p className="text-red-300 font-bold">{errors.diameter?.message}</p>
+        </>
+      )}
+      {type === "soup" && (
+        <>
+          <label htmlFor="spiciness_scale">Spiciness scale</label>
+          <input
+            type="range"
+            id="spiciness_scale"
+            min="1"
+            max="10"
+            {...register("spiciness_scale", {
+              required: { value: true, message: "Spiciness is required" },
+            })}
+          />
+          <p className="text-red-300 font-bold">
+            {errors.spiciness_scale?.message}
+          </p>
+        </>
+      )}
+      {type === "sandwich" && (
+        <>
+          <label htmlFor="slices_of_bread">Slices of bread</label>
+          <input
+            type="number"
+            min="1"
+            id="slices_of_bread"
+            {...register("slices_of_bread", {
+              required: {
+                value: true,
+                message: "Number of slices of bread is required",
+              },
+            })}
+          />
+          <p className="text-red-300 font-bold">
+            {errors.slices_of_bread?.message}
+          </p>
+        </>
+      )}
       <button>Submit</button>
     </form>
   );
